a7c4bc2d45b69430b25e6f628f33c6ba68304dd1
Dean Kerr <dean_k@live.co.uk>
2018-04-01 21:13:54 +0100

    feat: remove stories

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-// As a User i'd like to
-// [STRETCH] Sets
-// View my available sets
-// Select a set
-// Stickers
-// View my groups
-// Search my groups
-// Filter my groups
-// Friends
-// View my friends
-// Add a friend request
-// Accept a friend request
-// Delete a friend
 const styles = StyleSheet.create({
   container: {
     flex: 1,
