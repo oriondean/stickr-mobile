348f8b957615ef89acf1133eab4a27b20d1b50a8
Dean Kerr <dean_k@live.co.uk>
2018-04-01 20:33:44 +0100

    feat: update App component to eslint standards

--- App.js
+++ App.js
@@ -1,33 +1,29 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 // As a User i'd like to
 // [STRETCH] Sets
-  // View my available sets
-  // Select a set
+// View my available sets
+// Select a set
 // Stickers
-  // View my groups
-  // Search my groups
-  // Filter my groups
+// View my groups
+// Search my groups
+// Filter my groups
 // Friends
 // View my friends
 // Add a friend request
 // Accept a friend request
 // Delete a friend
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'flex-start',
+    alignItems: "stretch",
+    justifyContent: "flex-start"
   }
 });
+
+export default () => (
+  <View style={styles.container}>
+    <Text>Hello World</Text>
+  </View>
+);
